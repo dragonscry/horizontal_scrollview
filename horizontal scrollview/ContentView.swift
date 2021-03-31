ffb59eb9a1cf1a71512e1909b91db21bc9cafecc
Denis <denismerkotun@gmail.com>
2021-03-31 15:42:01 +0300

    Fix bug with card, adding top text

--- horizontal scrollview/ContentView.swift
+++ horizontal scrollview/ContentView.swift
@@ -12,12 +12,26 @@ struct ContentView: View {
         ZStack {
             Color(#colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1))
                 .ignoresSafeArea(.all)
-            ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4893727303, green: 0.07381700724, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.702722609, green: 0.4491981268, blue: 1, alpha: 1))]), startPoint: .bottomLeading, endPoint: .topTrailing)
-                Card()
+            
+            VStack{
+                Text("Current Ballance")
+                    .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                    .font(.system(size: 19, weight: .medium, design: .rounded))
+                Text("$11,502")
+                    .foregroundColor(.white)
+                    .font(.system(size: 35, weight: .bold, design: .rounded))
+                    .padding(.top, 4)
             }
-            .frame(width: 325, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .cornerRadius(25)
+            .offset(y: -250)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack (spacing: 20){
+                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                        Cards()
+                    }
+                }
+                .padding(.leading, 15.0)
+            }
         }
     }
 }
@@ -28,57 +42,63 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Card: View {
+
+struct Cards: View {
     var body: some View {
-        VStack {
-            
-            HStack{
-                //first four circles
-                HStack {
-                    ForEach (Range(1...3)) {_ in
-                        HStack {
-                            HStack (spacing: 5) {
-                                ForEach (Range(1...4)) {_ in
-                                    Circle()
-                                        .frame(width: 8, height: 8)
-                                        .foregroundColor(.white).opacity(0.7)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4893727303, green: 0.07381700724, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.702722609, green: 0.4491981268, blue: 1, alpha: 1))]), startPoint: .bottomLeading, endPoint: .topTrailing)
+            VStack {
+                
+                HStack{
+                    //first four circles
+                    HStack {
+                        ForEach (Range(1...3)) {_ in
+                            HStack {
+                                HStack (spacing: 5) {
+                                    ForEach (Range(1...4)) {_ in
+                                        Circle()
+                                            .frame(width: 8, height: 8)
+                                            .foregroundColor(.white).opacity(0.7)
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                .padding(.leading, 30)
-                Text("1234")
-                    .foregroundColor(.white).opacity(0.7)
-                    .font(.system(size: 13, weight: .bold, design: .rounded))
-                
-                Spacer()
-                
-            }
-            .padding(.top, 25)
-            
-            Spacer()
-            
-            HStack {
-                Text("$1,564")
-                    .font(.system(size: 25, weight: .bold, design: .rounded))
-                    .foregroundColor(.white)
                     .padding(.leading, 30)
-                Spacer()
-                
-                ZStack {
-                    Circle()
-                        .frame(width: 30, height: 30)
+                    Text("1234")
                         .foregroundColor(.white).opacity(0.7)
-                        .padding(.trailing, 40)
-                    Circle()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white).opacity(0.7)
+                        .font(.system(size: 13, weight: .bold, design: .rounded))
+                    
+                    Spacer()
                     
                 }
+                .padding(.top, 25)
                 
+                Spacer()
+                
+                HStack {
+                    Text("$1,564")
+                        .font(.system(size: 25, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                        .padding(.leading, 30)
+                    Spacer()
+                    
+                    ZStack {
+                        Circle()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white).opacity(0.7)
+                            .padding(.trailing, 40)
+                        Circle()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white).opacity(0.7)
+                        
+                    }
+                    
+                }
+                .padding(.bottom, 25)
             }
-            .padding(.bottom, 25)
         }
+        .frame(width: 325, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .cornerRadius(25)
     }
 }
